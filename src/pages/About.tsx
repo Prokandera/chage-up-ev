c1aa010a416a5254621798f6b0a43a7cf19b1eb5
SUNNY KUMAR <sunnykumar8585937257@gmail.com>
2025-11-25 22:59:16 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -67,7 +67,7 @@ const About = () => {
               </div>
               <div className="rounded-xl bg-muted/30 p-2 ring-1 ring-inset ring-border lg:flex lg:flex-col lg:justify-center animate-slide-in-right hover-lift">
                 <img
-                  src="https://images.unsplash.com/photo-1638887232075-33691d8de7d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                  src="/about-us-e1692957507489.png"
                   alt="Team working on electric vehicle charging solutions"
                   className="w-full rounded-md shadow-2xl image-parallax"
                 />
